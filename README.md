18cb8f9d50a41436708590294b7b635b4b8fb2a0
Oliver Warlow <oliver@warlow.engineer>
2024-07-02 19:18:02 +0000

    v1

---
+++ README.md
@@ -0,0 +1,3 @@
+# BeyondDice
+
+This module adds DnDBeyond dice skins to the Dice so Nice! module in FoundryVTT
---
+++ dice.js
@@ -0,0 +1,51 @@
+Hooks.once('diceSoNiceReady', (dice3d) => {
+	dice3d.addSystem({id: "clockwork", name: "Clockwork Dice Set"}, false);
+	dice3d.addDicePreset({
+		type: "d2",
+		labels: "",
+		modelFile: "modules/beyond-dice/dice/clockwork/d2.glb",
+		system: "clockwork"
+	});
+	dice3d.addDicePreset({
+		type: "d4",
+		labels: "",
+		modelFile: "modules/beyond-dice/dice/clockwork/d4.glb",
+		system: "clockwork"
+	});
+	dice3d.addDicePreset({
+		type: "d6",
+		labels: "",
+		modelFile: "modules/beyond-dice/dice/clockwork/d6.glb",
+		system: "clockwork"
+	});
+	dice3d.addDicePreset({
+		type: "d8",
+		labels: "",
+		modelFile: "modules/beyond-dice/dice/clockwork/d8.glb",
+		system: "clockwork"
+	});
+	dice3d.addDicePreset({
+		type: "d10",
+		labels: "",
+		modelFile: "modules/beyond-dice/dice/clockwork/d10.glb",
+		system: "clockwork"
+	});
+	dice3d.addDicePreset({
+		type: "d12",
+		labels: "",
+		modelFile: "modules/beyond-dice/dice/clockwork/d12.glb",
+		system: "clockwork"
+	});
+	dice3d.addDicePreset({
+		type: "d20",
+		labels: "",
+		modelFile: "modules/beyond-dice/dice/clockwork/d20.glb",
+		system: "clockwork"
+	});
+	dice3d.addDicePreset({
+		type: "d100",
+		labels: "",
+		modelFile: "modules/beyond-dice/dice/clockwork/d100.glb",
+		system: "clockwork"
+	});
+});
